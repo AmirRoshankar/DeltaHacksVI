3b0fabb1abc0ac018435e53b28fc1ae2759d385b
bkkbking <58105903+bkkbking@users.noreply.github.com>
2020-01-26 09:30:45 -0500

    colourDemo more edits

--- animation/colourDemo.py
+++ animation/colourDemo.py
@@ -1,4 +1,23 @@
 import plotly.express as px
 
-fig = px.choropleth(locations=["CA", "TX", "NY", "FL"], locationmode="USA-states", color=[1,2,3,5], scope="usa")
+
+fig = px.choropleth(locations=["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL",
+                               "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA",
+                               "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",
+                               "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI",
+                               "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"],
+                    locationmode="USA-states",
+                    color=[1,2,2,1,2,1,3,5,1,6,
+                           1,2,2,1,2,1,3,5,1,6,
+                           1,2,2,1,2,1,3,5,1,6,
+                           1,2,2,1,2,1,3,5,1,6,
+                           1,2,2,1,2,1,3,5,1,6,10],
+                    scope="usa",
+                    )
+
+fig.update_layout(
+    title_text='2001 US Hepatitis<br>(Hover for breakdown of score)',
+
+)
+
 fig.show()
