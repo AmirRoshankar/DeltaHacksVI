9881899b09c4556a408ad66b83bd4f2e57a9681f
bkkbking <58105903+bkkbking@users.noreply.github.com>
2020-01-26 10:15:27 -0500

    colourDemo for loop

--- animation/colourDemo.py
+++ animation/colourDemo.py
@@ -1,23 +1,18 @@
+import csv
 import plotly.express as px
+import random
 
+with open('states.csv', 'r') as file:
+    reader = csv.DictReader(file)
 
-fig = px.choropleth(locations=["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL",
-                               "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA",
-                               "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",
-                               "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI",
-                               "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"],
+    for row in reader:
+       fig = px.choropleth(locations=[row['state']],
                     locationmode="USA-states",
-                    color=[1,2,2,1,2,1,3,5,1,6,
-                           1,2,2,1,2,1,3,5,1,6,
-                           1,2,2,1,2,1,3,5,1,6,
-                           1,2,2,1,2,1,3,5,1,6,
-                           1,2,2,1,2,1,3,5,1,6,10],
-                    scope="usa",
-                    )
-
-fig.update_layout(
-    title_text='2001 US Hepatitis<br>(Hover for breakdown of score)',
-
-)
+                    color=[random.randint(0,10)],
+                    scope="usa")
+       break
+    fig.update_layout( title_text='2001 US Hepatitis<br>(Hover for breakdown of score)')
+file.close()
 
 fig.show()
+
