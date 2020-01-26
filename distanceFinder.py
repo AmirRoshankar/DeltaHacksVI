219e29a497730fd8246f6548682fb0cffabba416
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2020-01-26 10:36:08 -0500

    Made it a function

--- distanceFinder.py
+++ distanceFinder.py
@@ -2,41 +2,40 @@ import math
 
 # for the dict key, if key not in the dict, try flipping the order of the states
 
-# 1. open and read state coordinates
-statesFile = open("states.text", "r")
-statesCoords = statesFile.readlines()
-states = []
+def getDistances():
+    # 1. open and read state coordinates
+    statesFile = open("states.text", "r")
+    statesCoords = statesFile.readlines()
+    states = []
 
-# 2. split each line into substrings of state abbreviation, latitude, longitude
-for row in statesCoords:
-    row.strip("\n")
-    states.append(row.split("\t"))
+    # 2. split each line into substrings of state abbreviation, latitude, longitude
+    for row in statesCoords:
+        row.strip("\n")
+        states.append(row.split("\t"))
 
-# 3. convert coordinates into floats for distance calculation below in step 6
-for i in range(len(states)):
-    for j in range(1,3):
-        states[i][j] = float(states[i][j])
+    # 3. convert coordinates into floats for distance calculation below in step 6
+    for i in range(len(states)):
+        for j in range(1,3):
+            states[i][j] = float(states[i][j])
 
-# 4. initialize outer distances list
-distances = []
+    # 4. initialize outer distances list
+    distances = []
 
-# 5. initialize inner distances lists
-for i in range(len(states)):
-    distances.append([])
+    # 5. initialize inner distances lists
+    for i in range(len(states)):
+        distances.append([])
 
-# 6. calculate distances and append them to inner lists
-for i in range(len(states)):
-    for j in range(len(states)):
-        R = 6371000
-        phi1 = math.radians(states[i][1])
-        phi2 = math.radians(states[j][1])
-        phiDelta = math.radians(states[j][1] - states[i][1])
-        lambdaDelta = math.radians(states[j][2] - states[i][2])
+    # 6. calculate distances and append them to inner lists
+    for i in range(len(states)):
+        for j in range(len(states)):
+            R = 6371000
+            phi1 = math.radians(states[i][1])
+            phi2 = math.radians(states[j][1])
+            phiDelta = math.radians(states[j][1] - states[i][1])
+            lambdaDelta = math.radians(states[j][2] - states[i][2])
 
-        a = math.sin(phiDelta/2) * math.sin(phiDelta/2) + math.cos(phi1) * math.cos(phi2) * math.sin(lambdaDelta/2) * math.sin(lambdaDelta/2)
-        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-        d = R * c
+            a = math.sin(phiDelta/2) * math.sin(phiDelta/2) + math.cos(phi1) * math.cos(phi2) * math.sin(lambdaDelta/2) * math.sin(lambdaDelta/2)
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+            d = R * c
 
-        distances[i].append(d)
-
-print(distances)
+            distances[i].append(d)
