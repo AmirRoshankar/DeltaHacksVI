08e027990e0660f7eb0db621c734d5f31164653a
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2020-01-26 10:30:26 -0500

    Changed distanceFinder to using lists instead of dictioonaries

--- distanceFinder.py
+++ distanceFinder.py
@@ -17,29 +17,26 @@ for i in range(len(states)):
     for j in range(1,3):
         states[i][j] = float(states[i][j])
 
-# 4. initialize distances nested dictionary with first state nested as a dictionary
-distances = {states[0][0]: {}}
+# 4. initialize outer distances list
+distances = []
 
-# 5. initialize first dimension of distances (first states' distance dictionary)
+# 5. initialize inner distances lists
 for i in range(len(states)):
-    distances[states[i][0]] = {}
+    distances.append([])
 
-# 6. calculate distances and update secondary dictionaries
+# 6. calculate distances and append them to inner lists
 for i in range(len(states)):
     for j in range(len(states)):
-        for k in range(3):
-            R = 6371000
-            phi1 = math.radians(states[i][1])
-            phi2 = math.radians(states[j][1])
-            phiDelta = math.radians(states[j][1] - states[i][1])
-            lambdaDelta = math.radians(states[j][2] - states[i][2])
+        R = 6371000
+        phi1 = math.radians(states[i][1])
+        phi2 = math.radians(states[j][1])
+        phiDelta = math.radians(states[j][1] - states[i][1])
+        lambdaDelta = math.radians(states[j][2] - states[i][2])
 
-            a = math.sin(phiDelta/2) * math.sin(phiDelta/2) + math.cos(phi1) * math.cos(phi2) * math.sin(lambdaDelta/2) * math.sin(lambdaDelta/2)
-            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-            d = R * c
-            
-            if i != j:
-                distanceForCurrentState = {states[j][0]: d}
-                distances[states[i][0]].update(distanceForCurrentState)
+        a = math.sin(phiDelta/2) * math.sin(phiDelta/2) + math.cos(phi1) * math.cos(phi2) * math.sin(lambdaDelta/2) * math.sin(lambdaDelta/2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        d = R * c
+
+        distances[i].append(d)
 
 print(distances)
